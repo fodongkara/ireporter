04ee096bda591a465a572a96a5d021aba4d4ddb9
edna <edna@rr.com>
2019-03-02 15:16:21 +0300

    remove missplaced import

--- api/views/incident_routes.py
+++ api/views/incident_routes.py
@@ -3,7 +3,6 @@ from api.controllers.red_flag_controller import RedflagController
 from api.controllers.intervention_controller import InterventionController
 from api.authentication.auth import token_required, admin_required
 from api.controllers.intervention_controller import InterventionController
-from functools import wraps
 
 inc_controller = RedflagController()
 int_controller = InterventionController()
