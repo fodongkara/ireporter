d7ae9176d09eb88ddfb67459f264825cbdb1e4f2
Lamech Lwasampijja <llwasampijja@gmail.com>
2019-03-02 11:25:38 +0300

    Reverted changes in the gitignore file

--- api/__init__.py
+++ api/__init__.py
@@ -7,6 +7,6 @@ def create_app(config_name):
     app =  Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_name)
     app.register_blueprint(incident_blueprint, url_prefix="/api/v1")
-    app.register_blueprint(user_blueprint, url_prefix="/api/v1/auth")
+    app.register_blueprint(user_blueprint, url_prefix="/api/v1")
 
     return app
